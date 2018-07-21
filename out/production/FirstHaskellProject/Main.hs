e83f81f9934ac6bc899cd7062212e076185a1682
ToF <cthibaut@octo.com>
2018-07-21 22:42:14 +0200

    complete, not optimized

--- out/production/FirstHaskellProject/Main.hs
+++ out/production/FirstHaskellProject/Main.hs
@@ -1,6 +1,17 @@
-module Main where
+import System.Environment
+import WordLadder
 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    process args
+
+process :: [String] -> IO ()
+process ss | length ss < 3 = putStrLn "usage: Ladder <word-file> <start> <end>"
+process [file,start,end] = do
+    words <- fmap (filter (\w -> length w == length start)) $ fmap words $ readFile file
+    putStrLn $ unwords $ ladder words start end
+
+
